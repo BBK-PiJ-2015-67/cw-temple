4a0504d33899f6a9582cb72681b8b400d7520d18
Laurent Mignot <laurent@mignot.eu>
2016-04-04 16:00:48 +0100

    Naive implementation - finds the orb some of the time - no backtracking yet

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ PiJ Coursework 4 - George Osborne and the Temple of Gloom
     - Track how many times we've visited a square..?
 - Need to be able to backtrack if we get stuck
     - Some sort of collection of reachable squares "before" current square
+- Look at neighbours and evaluate closest to target - move there first..?    
 
 ## Escape
 - Use threading?
--- src/CavernExplorer/Explorator.java
+++ src/CavernExplorer/Explorator.java
@@ -1,6 +1,11 @@
 package CavernExplorer;
 
 import game.ExplorationState;
+import game.NodeStatus;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * @author lmignot
@@ -9,11 +14,38 @@ public class Explorator {
 
     private ExplorationState state;
 
+
     public Explorator(ExplorationState state) {
         this.state = state;
     }
 
     public void findTheOrb () {
+        Set<Long> seen = new LinkedHashSet<>();
 
+        while(state.getDistanceToTarget() > 0) {
+            Collection<NodeStatus> neighbours = state.getNeighbours();
+            // add current to seen items
+            seen.add(state.getCurrentLocation());
+            int distance = Integer.MAX_VALUE;
+            long next = -1L;
+            NodeStatus closest = getClosestNeighbour(neighbours, seen);
+
+            if (closest != null && closest.getDistanceToTarget() < distance) {
+                distance = closest.getDistanceToTarget();
+                next = closest.getId();
+            }
+
+            System.out.println("Moving to tile with id: " + next);
+            System.out.print("Moving from position: " + state.getCurrentLocation());
+            state.moveTo(next);
+            System.out.println("\tto: " + state.getCurrentLocation());
+        }
+    }
+
+    private NodeStatus getClosestNeighbour(Collection<NodeStatus> neighbours, Set<Long> visited) {
+        return neighbours.parallelStream()
+                .sorted(NodeStatus::compareTo)
+                .filter(n -> !visited.contains(n.getId()))
+                .findFirst().orElse(null);
     }
 }
