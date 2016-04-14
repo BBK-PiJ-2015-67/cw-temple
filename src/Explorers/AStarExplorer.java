a8cc82762398f07fdfffa57728c88673f5eab695
Laurent Mignot <laurent@mignot.eu>
2016-04-14 12:27:40 +0100

    trying to get damn A* explorer to work - however without x,y coordinates for each neighbour state I'm not sure it's possible... <sigh>

--- src/Explorers/AStarExplorer.java
+++ src/Explorers/AStarExplorer.java
@@ -1,94 +1,118 @@
 package Explorers;
 
 import game.ExplorationState;
-import student.PriorityQueue;
-import student.PriorityQueueImpl;
+import game.NodeStatus;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.function.Predicate;
 
 /**
  * @author lmignot
  */
 public class AStarExplorer implements CavernExplorer {
-    private static final double COST = 1.0;
-    private ExplorationState state;
 
-    private Set<ExploreNode> closed;
-    private PriorityQueue<ExploreNode> open;
+    private static final int MOVEMENT_COST = 1;
+
+    private ExplorationState state;
+    private Collection<ExploreNode> closed;
+    private Collection<ExploreNode> open;
+
+    private int costSoFar = 0;
 
     public AStarExplorer(ExplorationState state) {
         this.state = state;
-        open = new PriorityQueueImpl<>();
+        open = new LinkedHashSet<>();
         closed = new LinkedHashSet<>();
     }
 
     @Override
     public void findTheOrb() {
-        ExploreNode start = new ExploreNode(state.getCurrentLocation(), state.getDistanceToTarget(), 0);
-        open.add(start, 0);
+        ExploreNode start = new ExploreNode(state.getCurrentLocation(), state.getDistanceToTarget(), null);
+        start.setF(costSoFar);
+        open.add(start);
 
-        while(open.size() > 0 && state.getDistanceToTarget() != 0) {
-            ExploreNode current = open.poll();
+        while(!open.isEmpty() && state.getDistanceToTarget() != 0) {
+//            costSoFar++;
+            ExploreNode current = getNodeFrom(state.getCurrentLocation(), open);
+            open.remove(current);
+
+            for (NodeStatus n : state.getNeighbours()) {
+                ExploreNode tmp = new ExploreNode(n.getId(), n.getDistanceToTarget(), current);
+                tmp.setF(current.getF());
+
+                ExploreNode tmpOpen = getNodeFrom(tmp.getId(), open);
+                if (tmpOpen != null && tmpOpen.getF() < tmp.getF()) continue;
+
+                ExploreNode tmpClosed = getNodeFrom(tmp.getId(), closed);
+                if (tmpClosed != null && tmpClosed.getF() < tmp.getF()) continue;
+
+                open.add(tmp);
+            }
+
             closed.add(current);
 
-            state.getNeighbours()
-                    .stream()
-                    .filter(s -> !closed.parallelStream().anyMatch(n -> n.getId() == s.getId()))
-                    .forEach(s -> {
-                        ExploreNode n = new ExploreNode(s.getId(), s.getDistanceToTarget(), current.getPriority());
-                        open.add(n, n.getPriority());
-                    });
-
-            System.out.println(open.toString());
-            System.out.println("moving from tile " + current.getId() + " to tile " + open.peek().getId());
-            state.moveTo(open.peek().getId());
+            ExploreNode next = getSmallestF(open, state.getNeighbours());
+            if (next == null) {
+                next = getSmallestF(closed, state.getNeighbours());
+                if (next != null) {
+                    closed.remove(next);
+                    open.add(next);
+                    next.setF(10);
+                }
+            }
+            System.out.println("moving from tile " + current.getId() + " to tile " + next.getId());
+            state.moveTo(next.getId());
         }
     }
 
-    private static boolean nodeExistsIn(long id, Collection<ExploreNode> collection) {
+    private ExploreNode getNodeFrom(long id, Collection<ExploreNode> collection) {
         Predicate<ExploreNode> nodeExists = n -> n.getId() == id;
-        return collection.parallelStream().anyMatch(nodeExists);
+        return collection.parallelStream().filter(nodeExists).findFirst().orElse(null);
+    }
+
+    private ExploreNode getSmallestF(Collection<ExploreNode> collection, Collection<NodeStatus> neighbours) {
+        Comparator<ExploreNode> minF = (a, b) -> Integer.compare(a.getF(), b.getF());
+        return collection.parallelStream().filter(n -> isNeighbour(n.getId(), neighbours)).min(minF).orElse(null);
     }
 
     /**
      * ExploreNode
      */
     private class ExploreNode implements Comparable<ExploreNode> {
+        private ExploreNode parent;
         private long id;
         private int h;
-        private double g;
-        private double f;
+        private int f;
+        private int g = 0;
 
-        ExploreNode(long id, int distance, double p) {
+        ExploreNode(long id, int distance, ExploreNode parent) {
             this.id = id;
             h = distance;
-            g = p * COST;
-            f = g + h;
+            this.parent = parent;
         }
 
         long getId() {
             return id;
         }
 
-        double getPriority() {
+        int getF() {
             return f;
         }
 
-        void setPriority(int p) {
-            g += p;
+        void setF(int p) {
+            g += (p * MOVEMENT_COST);
             f = g + h;
+        }
+
+        ExploreNode getParent() {
+            return parent;
         }
 
         @Override
         public int compareTo(ExploreNode o) {
-            if (getPriority() == o.getPriority()) {
-                return 0;
-            } else {
-                return getPriority() < o.getPriority() ? 1 : -1;
-            }
+            return Integer.compare(getF(), o.getF());
         }
     }
 }
